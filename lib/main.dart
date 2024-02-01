0a7754f81c182b863330303a30c05d755cdecf79
Mohamad abdUlaziz <mohamad.samer.abdulaziz@gmail.com>
2024-02-01 13:48:42 +0300

    add speech to text pkg,refactoring sample code

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_tex/speech_to_text.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SpeechSampleApp(),
+    );
+  }
+}
